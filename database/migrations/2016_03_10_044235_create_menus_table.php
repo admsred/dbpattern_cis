fa2a98a77b88f44e1bb54cdfaa945f362a2e08a0
eko <eko.purnomo@gmail.com>
2016-03-10 11:59:07 +0700

    create table menus

---
+++ database/migrations/2016_03_10_044235_create_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sys_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',100);
+            $table->text('url');
+            $table->integer('parent')->nullable()->unsigned();
+            $table->integer('position')->unsigned();
+            $table->string('icon',100);
+            $table->enum('status',['on','off'])->index();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('"0000-00-00 00:00:00" on update CURRENT_TIMESTAMP'));
+            $table->string('author',50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sys_menus');
+    }
+}
