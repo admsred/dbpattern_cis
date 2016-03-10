204c58b4ef18fc50ab2396a06d8877c9fc9cbc6b
eko <eko.purnomo@gmail.com>
2016-03-10 10:55:20 +0700

    update field profile table

--- database/migrations/2016_03_08_045956_create_profile_table.php
+++ database/migrations/2016_03_08_045956_create_profile_table.php
@@ -15,7 +15,13 @@ class CreateProfileTable extends Migration
         Schema::create('sys_profile', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',100);
+            $table->string('appname',100);
+            $table->string('npwp',50);
             $table->string('phone',50);
+            $table->string('fax',50);
+            $table->string('email',150);
+            $table->text('address');
+            $table->string('logo');
             $table->timestamp('crated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('"0000-00-00 00:00:00" on update CURRENT_TIMESTAMP'));
             $table->string('author',50);
